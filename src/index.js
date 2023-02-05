8bb48594ec2f43d5fa63f0efb9e73bf831f22779
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-02-05 18:28:20 +0800

    update index.js and style

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import '@/styles/index.scss'
 document.addEventListener('DOMContentLoaded', UI.initAddTaskButton)
 document.addEventListener('DOMContentLoaded', UI.initAddProjectButton)
 document.addEventListener('DOMContentLoaded', UI.initProjectButtons)
+document.addEventListener('DOMContentLoaded', UI.initTaskButtons)
